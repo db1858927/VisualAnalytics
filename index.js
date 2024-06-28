0473940fbe64ecef9f0a2cf3b5e0641f48800f2e
Elisa De Bellis <debellis.1858927@studenti.uniroma1.it>
2024-06-28 10:15:31 +0200

    server-side fix

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const express = require("express")
-const d3 = requinre('d3')
+const express = require('express')
+const d3 = require('d3')
 
 const port = 1234
 
-//create the server object
+// create the server object
 const server = express()
 
-//set an endpoint
-server.get('/', (req,res) => {
-    res.send('Hello World!')
+// set an endpoint
+server.get('/', (req, res) => {
+  res.send('Hello World!')
 })
 
-//start the server
+// start the server
 server.listen(port, () => {
-    console.log(`Server active on port ${port}`)
+  console.log(`Server active on port ${port}`)
 })
